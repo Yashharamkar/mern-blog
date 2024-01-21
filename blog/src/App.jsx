11974988c4fb04aa96be41f488fb36be79fb9d82
yash haramkar <yashharamkar2002@gmail.com>
2024-01-21 08:41:28 +0530

    create and add the footer components

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 
@@ -24,6 +25,7 @@ export default function App() {
 
      
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
--- blog/src/pages/SignUp.jsx
+++ blog/src/pages/SignUp.jsx
@@ -1,8 +1,30 @@
 import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 export default function SignUp() {
+ const [formData,setformData]=useState({});
+
+    const handleChange=(e)=>{
+      setformData({...formData,[e.target.id]: e.target.value});
+    };
+    
+    const handleSubmit=async(e)=>{
+      e.preventDefault();
+      try{
+        const res=await fetch('/api/auth/signup',{
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json'},
+          body: JSON.stringify(formData),
+        });
+        const data=await res.json();
+      }catch(error){}
+
+      
+    }
+
+
   return (
     <div className='min-h-screen mt-20'>
       <div className=' flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -17,29 +39,29 @@ export default function SignUp() {
         </div>
 
         <div className='flex-1'>
-          <form  className='flex flex-col gap-4'>
+          <form  className='flex flex-col gap-4 'onSubmit={handleSubmit}>
             <div>
               <Label value='Your username'/>
               <TextInput
               type='text'
               placeholder='Username'
-              id='username'
+              id='username'onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your email'/>
               <TextInput
-              type='text'
+              type='email'
               placeholder='name@company.com'
-              id='email'
+              id='email'onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your password'/>
               <TextInput
-              type='text'
+              type='password'
               placeholder='Password'
-              id='password'
+              id='password'onChange={handleChange}
               />
             </div>
             <Button gradientDuoTone='purpleToPink' type='submit'>
