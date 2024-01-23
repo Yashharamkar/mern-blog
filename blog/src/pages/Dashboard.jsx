f29687d62454c6a8756f054209367ff20878c36b
yash haramkar <yashharamkar2002@gmail.com>
2024-01-23 08:26:41 +0530

    make dasboard private and edit google Oauth

--- blog/src/pages/Dashboard.jsx
+++ blog/src/pages/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom'
+import DashSideBar from '../components/DashSideBar'
+import DashProfile from '../components/DashProfile'
+
 
 export default function Dashboard() {
-  return (
-    <div>
-      Dashboard
+  const location=useLocation();
+  const [tab,setTab]=useState('')
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search)
+    const tabFromUrl=urlParams.get('tab')
+  if(tabFromurl){
+    setTab(tabfromUrl);
+  }
+
+  },[location.search]);
+  return (<div>
+    <div className=''>
+      {/*Sidebar*/}
+    <DashSidebar/>
+    </div>
+    {/*profile..*/}
+    {tab==='profile'&& <DashProfile/>}
+
     </div>
   )
 }
